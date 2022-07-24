cdbe5126f29db14a9beac28fdedff2aa944ea3b7
yoshisansan <awakewalker007@gmail.com>
2022-07-24 22:57:02 +0900

    env調整

--- components/util/seo.tsx
+++ components/util/seo.tsx
@@ -48,7 +48,7 @@ const SEO: FC<{
           type: pageType,
           images: [
             {
-              url: ThumbNail.src,
+              url: `${process.env.NEXT_PUBLIC_HOST}/${ThumbNail.src}`,
               width: 1200,
               height: 630,
               alt: `ロンギヌスの槍を投げ放題サイトのサムネイル画像`
