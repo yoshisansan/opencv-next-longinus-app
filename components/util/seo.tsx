4a29f33c36a54c87a63fd4605cdaf9b825e8820f
yoshisansan <awakewalker007@gmail.com>
2022-07-26 22:06:12 +0900

    不要なコメントアウト消去

--- components/util/seo.tsx
+++ components/util/seo.tsx
@@ -30,18 +30,6 @@ const SEO: FC<{
           }}
         />
       </Head>
-      {/* <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-      />
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { 'page_path': ${url}, 'send_page_view': true });
-          `}
-      </Script> */}
       <NextSeo
         title={title}
         description={description}
