fe03cf1f77d09bd81c1f8905789596ab81fa54d3
yoshisansan <awakewalker007@gmail.com>
2022-07-26 21:26:45 +0900

    tesss

--- components/util/seo.tsx
+++ components/util/seo.tsx
@@ -21,14 +21,13 @@ const SEO: FC<{
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         />
-
         <script
           dangerouslySetInnerHTML={{
             __html: `
    window.dataLayer = window.dataLayer || [];
    function gtag(){dataLayer.push(arguments);}
    gtag('js', new Date());
-   gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { page_path: window.location.href, 'send_page_view': true });
+   gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { page_path: ${url}, 'send_page_view': true });
             `
           }}
         />
